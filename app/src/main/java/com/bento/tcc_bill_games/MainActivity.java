c4bf11dbf3ccdbe74ed85ab501dd279779b42172
baruclincoln@hotmail.com <baruc2001>
2019-07-30 09:55:48 -0300

    Layouts changed

--- app/src/main/java/com/bento/tcc_bill_games/MainActivity.java
+++ app/src/main/java/com/bento/tcc_bill_games/MainActivity.java
@@ -2,13 +2,49 @@ package com.bento.tcc_bill_games;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    //UI variables
+    private Button btn_search;
+    private Button btn_user;
+    private Button btn_projects;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_search = findViewById(R.id.btn_search);
+        btn_user = findViewById(R.id.btn_user_profile);
+        btn_projects = findViewById(R.id.btn_projects);
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_projects.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
