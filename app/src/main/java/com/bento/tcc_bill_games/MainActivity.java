65ab05e34c9f6560715da0455785bda908582695
baruclincoln@hotmail.com <baruc2001>
2019-07-31 11:42:59 -0300

    Login is working

--- app/src/main/java/com/bento/tcc_bill_games/MainActivity.java
+++ app/src/main/java/com/bento/tcc_bill_games/MainActivity.java
@@ -7,11 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
     //UI variables
     private Button btn_search;
     private Button btn_user;
     private Button btn_projects;
+    private Button btn_logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         btn_search = findViewById(R.id.btn_search);
         btn_user = findViewById(R.id.btn_user_profile);
         btn_projects = findViewById(R.id.btn_projects);
+        btn_logout = findViewById(R.id.btn_logout);
 
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         btn_user.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
                 startActivity(intent);
             }
         });
@@ -40,11 +44,31 @@ public class MainActivity extends AppCompatActivity {
         btn_projects.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MessageActivity.class);
+                Intent intent = new Intent(MainActivity.this, ProjectActivity.class);
                 startActivity(intent);
             }
         });
 
 
+
+        verifyAuthentication();
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                verifyAuthentication();
+            }
+        });
+    }
+
+    private void verifyAuthentication() {
+        if(FirebaseAuth.getInstance().getUid() == null){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            startActivity(intent);
+
+        }
     }
 }
